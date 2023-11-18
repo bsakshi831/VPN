6ec192f1135e612fd5b38398200e83ba96e66e82
Sakshi Bhongade <90645072+bsakshi831@users.noreply.github.com>
2023-11-18 16:06:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def connect_to_vpn():
+    server_address = entry_server.get()
+    username = entry_username.get()
+    password = entry_password.get()
+    
+    if server_address and username and password:
+        #simulate a connection to VPN server
+        messagebox.showinfo("Connected", "Connected to VPN server")
+    else:
+        messagebox.showerror("Error", "Please fill in all the fields")
+
+#create the main window
+root = tk.Tk()
+root.title(" VPN ")
+
+# server address
+label_server = tk.Label(root, text="Server Address")
+label_server.pack()
+entry_server = tk.Entry(root)
+entry_server.pack()
+
+# Username
+label_username = tk.Label(root, text="Username")
+label_username.pack()
+entry_username = tk.Entry(root)
+entry_username.pack()
+
+# Password
+label_password = tk.Label(root, text="Password")
+label_password.pack()
+entry_password = tk.Entry(root, show="*")
+entry_password.pack()
+
+# connect the button
+connect_button = tk.Button(root, text="Connect", command=connect_to_vpn)
+connect_button.pack()
+
+# run the main loop
+root.mainloop()
